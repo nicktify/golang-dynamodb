be4dc85501395deee78da33e1ee29c53568e39a8
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2022-12-24 20:23:36 -0300

    Golang server to read data from DynamoDB

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/joho/godotenv"
+)
+
+
+type Value struct {
+    Value string `dynamodbav:"value" json:"value"`
+    Timestamp string `dynamodbav:"timestamp" json:"timestamp"`
+}
+
+type Device struct {
+     DeviceID string `dynamodbav:"deviceID" json:"deviceID"`
+     Testing string `dynamodbav:"testing" json:"testing"`
+     Flow []Value `dynamodbav:"last_flow" json:"last_flow"`
+}
+
+type Person struct {
+    name string
+    age  int
+}
+
+type GetDeviceBody struct {
+    DeviceID string
+}
+
+func getItem(deviceID string)(Device, error) {
+    var device Device
+
+    cfg, err := config.LoadDefaultConfig(context.TODO())
+    client := dynamodb.NewFromConfig(cfg)
+
+    env, _ := godotenv.Read(".env")
+
+    response, err := client.GetItem(context.Background(),
+	&dynamodb.GetItemInput{
+	    TableName: aws.String(env["DEVICE_FILTER_TABLENAME"]),
+	    Key: map[string]types.AttributeValue{
+		"deviceID": &types.AttributeValueMemberS{Value: *aws.String(deviceID)},
+	    },
+	})
+    if err != nil {
+	return device, nil
+    }
+
+    if response.Item == nil {
+	return device, nil
+    }
+
+    err = attributevalue.UnmarshalMap(response.Item, &device)
+
+    if err != nil {
+	return device, nil
+    }
+
+    return device, nil
+}
+
+func getDevice(w http.ResponseWriter, req *http.Request) {
+    var body GetDeviceBody
+
+    fmt.Println(req.Body)
+
+    err := json.NewDecoder(req.Body).Decode(&body)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    device, err := getItem(body.DeviceID)
+
+    fmt.Println(body)
+
+    json.NewEncoder(w).Encode(device)
+}
+
+func main() {
+    http.HandleFunc("/get-device", getDevice)
+    http.ListenAndServe(":4000", nil)
+}
+
